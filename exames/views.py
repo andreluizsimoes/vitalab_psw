fa3415e03bf7b7b5d4955c2f3667f0a7be29c3f3
André Simões <andresimoes2704@gmail.com>
2023-10-08 00:29:18 -0300

    aula 02 ok!

--- exames/views.py
+++ exames/views.py
@@ -86,3 +86,45 @@ def cancelar_pedido(request, pedido_id):
     pedido.save()
     messages.add_message(request, constants.SUCCESS, 'Pedido cancelado com sucesso!')
     return redirect('/exames/gerenciar_pedidos/')
+
+@login_required
+def gerenciar_exames(request):
+    exames = SolicitacaoExame.objects.filter(usuario=request.user)
+
+    return render(request, 'gerenciar_exames.html', {'exames': exames})
+
+@login_required
+def permitir_abrir_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+		#TODO: validar se o exame é do usuário
+    if not exame.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'EPA! Não pode visualizar exame que não é seu!')
+        return redirect('/exames/gerenciar_exames/')
+
+    if not exame.requer_senha:
+        # verificar se o pdf existe
+        if exame.resultado:
+            return redirect(exame.resultado.url)
+        else:
+            messages.add_message(request, constants.ERROR, 'Não há arquivo cadastrado em nossa base de dados')
+            return redirect('/exames/gerenciar_exames/')
+    else: 
+        return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
+    
+@login_required
+def solicitar_senha_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    if not exame.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'EPA! Não pode visualizar exame que não é seu!')
+        return redirect('/exames/gerenciar_exames/')
+    
+    if request.method == "GET":
+        return render(request, 'solicitar_senha_exame.html', {'exame': exame})
+    elif request.method == "POST":
+        senha = request.POST.get("senha")
+				#TODO: validar se o exame é do usuário
+        if senha == exame.senha:
+            return redirect(exame.resultado.url)
+        else:
+            messages.add_message(request, constants.ERROR, 'Senha inválida!')
+            return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
